78314802fd59008f5f20e6c97da44a10fdcf44b4
Rafael Maciel <rafaelmaciel@Rafaels-MacBook-Pro.local>
2024-06-09 16:35:27 -0300

    wip

--- components/shared/location/Card.vue
+++ components/shared/location/Card.vue
@@ -21,10 +21,11 @@ const icon = computed(() => {
     to=""
     class="col-span-1 border-2 p-4 rounded-xl hover:scale-105 scale-100 transition-all cursor-pointer flex flex-col items-start justify-start shadow-lg hover:bg-actions-foreground hover:text-background gap-3"
   >
-    <p class="flex gap-4 text-xl font-bold">
-      {{ props.location.name }}: 1 - 10
+    <div class="flex items-center justify-between w-full">
       <Icon :icon="`game-icons:${icon}`" class="w-8 h-8 cursor-pointer" />
-    </p>
+      <p class="text-md font-bold">1 - 10</p>
+    </div>
+    <p class="flex gap-4 text-xl font-bold">{{ props.location.name }}</p>
     <div class="w-full pt-2">
       <div class="gap-4 pb-2 flex flex-wrap">
         <Icon
